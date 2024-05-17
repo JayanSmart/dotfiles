d23dd33173d24fb002d7393448640edd7eb4b9bf
Jayan Smart <jayandrinsmart@gmail.com>
2024-05-17 15:52:20 +0100

    fix telescope comand descriptions breaking nvim

--- modules/common/neovim/config/telescope.lua
+++ modules/common/neovim/config/telescope.lua
@@ -1,21 +1,29 @@
 local telescope = require("telescope.builtin")
-vim.keymap.set("n", "<Leader>k", telescope.keymaps, desc = "keymaps")
-vim.keymap.set("n", "<Leader>/", telescope.live_grep, desc = "live grep")
-vim.keymap.set("n", "<Leader>ff", telescope.find_files, desc = "find files")
-vim.keymap.set("n", "<Leader>fp", telescope.git_files, desc = "git files")
-vim.keymap.set("n", "<Leader>fw", telescope.grep_string, desc = "grep for string")
-vim.keymap.set("n", "<Leader>b", telescope.buffers, desc = "search buffers")
-vim.keymap.set("n", "<Leader>hh", telescope.help_tags, desc = "help tags")
-vim.keymap.set("n", "<Leader>fr", telescope.oldfiles, desc = "oldfiles")
-vim.keymap.set("n", "<Leader>cc", telescope.commands, desc = "commands")
-vim.keymap.set("n", "<Leader>gc", telescope.git_commits, desc = "git commits")
-vim.keymap.set("n", "<Leader>gf", telescope.git_bcommits, desc = "git bcommits")
-vim.keymap.set("n", "<Leader>gb", telescope.git_branches, desc = "git branches")
-vim.keymap.set("n", "<Leader>gs", telescope.git_status, desc = "git status")
-vim.keymap.set("n", "<Leader>s", telescope.current_buffer_fuzzy_find, desc = "current buffer")
-vim.keymap.set("n", "<Leader>rr", telescope.resume, desc = "resume")
 
-vim.keymap.set("n", "<Leader>N", function()
+
+-- Function to cut down config boilerplate
+local key = function(mode, key_sequence, action, params)
+    params = params or {}
+    vim.keymap.set(mode, key_sequence, action, params)
+end
+
+key("n", "<Leader>k", telescope.keymaps, { desc = "keymaps" })
+key("n", "<Leader>/", telescope.live_grep, { desc = "live grep" })
+key("n", "<Leader>ff", telescope.find_files, { desc = "find files" })
+key("n", "<Leader>fp", telescope.git_files, { desc = "git files" })
+key("n", "<Leader>fw", telescope.grep_string, { desc = "grep for string" })
+key("n", "<Leader>b", telescope.buffers, { desc = "search buffers" })
+key("n", "<Leader>hh", telescope.help_tags, { desc = "help tags" })
+key("n", "<Leader>fr", telescope.oldfiles, { desc = "oldfiles" })
+key("n", "<Leader>cc", telescope.commands, { desc = "commands" })
+key("n", "<Leader>gc", telescope.git_commits, { desc = "git commits" })
+key("n", "<Leader>gf", telescope.git_bcommits, { desc = "git bcommits" })
+key("n", "<Leader>gb", telescope.git_branches, { desc = "git branches" })
+key("n", "<Leader>gs", telescope.git_status, { desc = "git status" })
+key("n", "<Leader>s", telescope.current_buffer_fuzzy_find, { desc = "current buffer" })
+key("n", "<Leader>rr", telescope.resume, { desc = "resume" })
+
+key("n", "<Leader>N", function()
     local opts = {
         prompt_title = "Search Notes",
         cwd = "$NOTES_PATH",
@@ -23,7 +31,7 @@ vim.keymap.set("n", "<Leader>N", function()
     telescope.live_grep(opts)
 end)
 
-vim.keymap.set("n", "<Leader>fN", function()
+key("n", "<Leader>fN", function()
     local opts = {
         prompt_title = "Find Notes",
         cwd = "$NOTES_PATH",
@@ -31,7 +39,7 @@ vim.keymap.set("n", "<Leader>fN", function()
     telescope.find_files(opts)
 end)
 
-vim.keymap.set("n", "<Leader>cr", function()
+key("n", "<Leader>cr", function()
     local opts = require("telescope.themes").get_ivy({
         layout_config = {
             bottom_pane = {
@@ -43,11 +51,11 @@ vim.keymap.set("n", "<Leader>cr", function()
 end)
 
 -- Zoxide
-vim.keymap.set("n", "<Leader>fz", require("telescope").extensions.zoxide.list)
+key("n", "<Leader>fz", require("telescope").extensions.zoxide.list)
 
 -- Project
 require("telescope").load_extension("projects")
-vim.keymap.set("n", "<C-p>", function()
+key("n", "<C-p>", function()
     local opts = require("telescope.themes").get_ivy({
         layout_config = {
             bottom_pane = {
@@ -60,8 +68,8 @@ end)
 
 -- File browser
 require("telescope").load_extension("file_browser")
-vim.keymap.set("n", "<Leader>fa", require("telescope").extensions.file_browser.file_browser)
-vim.keymap.set("n", "<Leader>fD", function()
+key("n", "<Leader>fa", require("telescope").extensions.file_browser.file_browser)
+key("n", "<Leader>fD", function()
     local opts = {
         prompt_title = "Find Downloads",
         cwd = "~/downloads",
